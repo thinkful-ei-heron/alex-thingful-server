11aa0154ea3d4b81a94013f0cf1dfc344f38136a
Alex Bannow <rbannal@gmail.com>
2019-11-15 18:17:52 +0000

    added jwt

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,22 +1,19 @@
-const bcrypt = require('bcryptjs')
-const AuthService = require('../auth/auth-service')
+const AuthService = require('../auth/auth-service');
 
 function requireAuth(req, res, next) {
-  const authToken = req.get('Authorization') || ''
-  console.log(authToken)
+  const authToken = req.get('Authorization') || '';
 
-  let basicToken
-
-  if(!authToken.toLowerCase().startsWith('basic ')) {
-    return res.status(401).json({ error: 'Missing basic token' })
+  let basicToken;
+  if (!authToken.toLowerCase().startsWith('basic ')) {
+    return res.status(401).json({ error: 'Missing basic token' });
   } else {
-    basicToken = authToken.slice('basic '.length, authToken.length)
+    basicToken = authToken.slice('basic '.length, authToken.length);
   }
 
-  const [tokenUserName, tokenPassword] = AuthService.parseBasicToken(basicToken)
+  const [tokenUserName, tokenPassword] = AuthService.parseBasicToken(basicToken);
 
-  if(!tokenUserName || !tokenPassword) {
-    return res.status(401).json({ error: 'Unauthorized request' })
+  if (!tokenUserName || !tokenPassword) {
+    return res.status(401).json({ error: 'Unauthorized request' });
   }
 
   AuthService.getUserWithUserName(
@@ -25,21 +22,21 @@ function requireAuth(req, res, next) {
   )
     .then(user => {
       if (!user) {
-        return res.status(401).json({ error: 'Unauthorized request' })
+        return res.status(401).json({ error: 'Unauthorized request' });
       }
-      return bcrypt.compare(tokenPassword, user.password)
-        .then(passwordsMatch => {
-          if(!passwordsMatch) {
-            return res.status(401).json({ error: 'Unauthorized request' })
+
+      return AuthService.comparePasswords(tokenPassword, user.password)
+        .then(passwordMatch => {
+          if(!passwordMatch) {
+            return res.status(401).json({ error: 'Unauthorized request' });
           }
-          
-          req.user = user
-          next()
-        })
+          req.user = user;
+          next();
+        });
     })
-    .catch(next)
+    .catch(next);
 }
 
 module.exports = {
-  requireAuth
-}
+  requireAuth,
+};
